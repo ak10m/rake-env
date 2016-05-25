fe35fd5488852b0d6b13645fe89d55736e00dfe1
Akio Morimoto <akio.morimoto@airits.jp>
2016-05-25 13:49:22 +0900

    initial commit

---
+++ lib/rake/env.rb
@@ -0,0 +1,7 @@
+require "rake/env/version"
+
+module Rake
+  module Env
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rake/env'
+
+require 'minitest/autorun'
